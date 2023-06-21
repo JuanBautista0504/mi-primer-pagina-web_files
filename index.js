448b8364ed4474d9735784de46c029007cb47041
JuanBautista0504 <136765322+JuanBautista0504@users.noreply.github.com>
2023-06-21 19:54:00 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const miBoton = document.getElementById('miBoton');
+function handleClick() {
+    alert('¡Hola! Has hecho ckick en el boton.');
+}
---
+++ script.js
@@ -0,0 +1,14 @@
+function showAlert(){ alert('¡Hola, esta es una alerta desde JavaSript!'); }
+let boton = document.getElementById("miBoton");
+boton.sddEventListener("click", function() {
+    alert("¡haz hecho click en el boton!");
+});
+document.querySelector("button..button-menu-toggle")
+.addEventListener("click", function() {
+
+
+    document.querySelector(".nav-links").
+
+
+    classList.toggle("nav-links-responsive")
+})
